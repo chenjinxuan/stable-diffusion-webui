ce544f30e9e429ce162cd6a4dccf7cc9976e96e3
jinxuan.chen <jinxuan.chen@pixocial.com>
2023-03-24 12:13:48 +0800

    fix

--- modules/txt2img.py
+++ modules/txt2img.py
@@ -6,7 +6,7 @@ from modules.shared import opts, cmd_opts
 import modules.shared as shared
 import modules.processing as processing
 from modules.ui import plaintext_to_html
-from gradio.tools import JSCode
+import gradio as gr
 
 
 def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2: str, steps: int, sampler_index: int, restore_faces: bool, tiling: bool, n_iter: int, batch_size: int, cfg_scale: float, seed: int, subseed: int, subseed_strength: float, seed_resize_from_h: int, seed_resize_from_w: int, seed_enable_extras: bool, height: int, width: int, enable_hr: bool, denoising_strength: float, firstphase_width: int, firstphase_height: int, *args):
@@ -59,5 +59,5 @@ def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2:
 
     if opts.do_not_show_images:
         processed.images = []
-    JSCode("window.my_var = {};", processed.images)
+    gr.Interface.show_input("Your input is: " + p.sd_model)
     return processed.images, generation_info_js, plaintext_to_html(processed.info)
